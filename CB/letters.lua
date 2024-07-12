3f0cba029cb3ce5657b0e96406d6ffac57c56119
zestycodes <118841160+zesty-codes@users.noreply.github.com>
2024-07-12 20:38:18 +0200

    Create letters.lua

---
+++ CB/letters.lua
@@ -0,0 +1,18 @@
+    local voidfunction = function()
+        return nil
+    end
+    local void = {__index = voidfunction}
+
+    local Cyrillic = setmetatable({
+        --[=[
+            Lowercased
+        --]=]
+        a = "а"; c = "с"; e = "е"; h = "һ"; l = "ӏ"; o = "о"; p = "р"; s = "ѕ"; y = "у";
+        
+        --[=[
+            Uppercased
+        --]=]
+        A = "А"; B = "В"; C = "С"; E = "Е"; H = "Н"; L = "ӏ"; O = "О"; P = "Р"; T = "Т"; M = "М"; N = "Н"; K = "К"; S = "Ѕ";
+    }, void)
+
+    return Cyrillic
